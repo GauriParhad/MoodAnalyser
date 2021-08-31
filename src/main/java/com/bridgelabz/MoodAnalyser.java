6e31177ce91f42fa88ae3b5bffffe2c3ed0fe0c8
Gaurip10 <thegauriparhad@gmail.com>
2021-08-31 18:43:38 +0530

    Added: Welcome tomood analyser program

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+    public void analysemood(String mood){
+        System.out.println("Current Mood is"+mood);
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Mood analyser to analyze Happy or sad Mood.");
+        MoodAnalyser obj = null MoodAnalyser();
+        obj.analysemood("Happy");
+    }
+}
